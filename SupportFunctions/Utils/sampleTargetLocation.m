3114f350e68fc0dbb91a2e7fd8f061e4957cdc4d
Jake <jacoby8s@gmail.com>
2014-01-17 13:51:33 -0600

    sampleTargetLocation - fixed resampling when a target is randomly drawn too close to fixation pdsEyelinkSetup - added custom calibration points with a scale variable. This only seems to work with the 32bit eyelink toolbox. Need to email somebody. pdsSpikeServerGetSpikes - added a check to see if spike server is present for this computer before running

--- SupportFunctions/Utils/sampleTargetLocation.m
+++ SupportFunctions/Utils/sampleTargetLocation.m
@@ -16,31 +16,37 @@ if isempty(gridx)
 else
     xi = round(unifrnd(1,gridx));
     yi = round(unifrnd(1,gridx));
-
-xlocs = round(linspace(rect(1),rect(3), gridx));
-ylocs = round(linspace(rect(2),rect(4), gridx));
-
-
-
-
-x = xlocs(xi);
-y = ylocs(yi);
-
+    
+    xlocs = round(linspace(rect(1),rect(3), gridx));
+    ylocs = round(linspace(rect(2),rect(4), gridx));
+    
+    
+    
+    
+    x = xlocs(xi);
+    y = ylocs(yi);
+    
 end
 
 maxIter = 50;
 iter = 1;
 
 while sqrt(x^2 + y^2) < 150 && iter < maxIter
-    xi = round(unifrnd(1,gridx));
-    yi = round(unifrnd(1,gridx));
     
-    x = xlocs(xi);
-    y = ylocs(yi);
+    if isempty(gridx)
+        x = rect(1) + (rect(3)-rect(1))*rand;
+        y = rect(2) + (rect(4)-rect(2))*rand;
+    else
+        xi = round(unifrnd(1,gridx));
+        yi = round(unifrnd(1,gridx));
+        
+        x = xlocs(xi);
+        y = ylocs(yi);
+    end
     
     iter = iter+1;
 end
-    
+
 
 
 xy = [x -y];
