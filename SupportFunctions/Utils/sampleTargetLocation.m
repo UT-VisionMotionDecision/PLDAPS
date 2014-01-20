1a0eb1d80faa5154a09750d45c6f201a7fe977ec
Jake <jacoby8s@gmail.com>
2014-01-20 12:37:47 -0600

    fixed perifoveal boundry for sampling target

--- SupportFunctions/Utils/sampleTargetLocation.m
+++ SupportFunctions/Utils/sampleTargetLocation.m
@@ -31,15 +31,16 @@ end
 maxIter = 50;
 iter = 1;
 
-while sqrt(x^2 + y^2) < 150 && iter < maxIter
-    xi = round(unifrnd(1,gridx));
-    yi = round(unifrnd(1,gridx));
-    
-    x = xlocs(xi);
-    y = ylocs(yi);
-    
-    iter = iter+1;
-end
+% commented out. ktz Jan 15 2014.
+% while sqrt(x^2 + y^2) < 150 && iter < maxIter
+%     xi = round(unifrnd(1,gridx));
+%     yi = round(unifrnd(1,gridx));
+%     
+%     x = xlocs(xi);
+%     y = ylocs(yi);
+%     
+%     iter = iter+1;
+% end
     
 
 
