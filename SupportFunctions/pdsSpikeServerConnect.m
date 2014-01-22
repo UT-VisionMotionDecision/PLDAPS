94d1d536f882e6b9b7d3d55360490c019ee1ac01
Jake <jacoby8s@gmail.com>
2014-01-22 16:05:02 -0600

    added dv.useSpikeServer checks to pdsSpikeServerConnect

--- SupportFunctions/pdsSpikeServerConnect.m
+++ SupportFunctions/pdsSpikeServerConnect.m
@@ -1,4 +1,4 @@
-function dv = spikeServerConnect(dv)
+function dv = pdsSpikeServerConnect(dv)
 % dv = spikeServerConnect(dv)
 %
 % INPUTS
@@ -11,28 +11,35 @@ function dv = spikeServerConnect(dv)
 % OUTPUTS
 %       dv [struct]
 %           .spikeserver [struct] - spikeserver subfield
-%    modified fields: 
+%    modified fields:
 %               .sock
-%               .isConnected 
+%               .isConnected
 %               .t0 - time of connection (server)
 %               .localt0 - time of connection (local)
 
+if ~isfield(dv,'useSpikeServer')
+    dv.useSpikeServer = 0;
+    fprintf('spike server is turned off. If you intend it to be on, update your rig file to include dv.useSpikeserver = 1\r')
+end
 
-try
-    [dv.spikeserver.sock,dv.spikeserver.isConnected, dv.spikeserver.t0, dv.spikeserver.localt0] = udpConnect(dv.spikeserver.selfip,dv.spikeserver.selfport,dv.spikeserver.remoteip,dv.spikeserver.remoteport);
-    if dv.spikeserver.isConnected ==0
-        disp('***********************************************************')
-        disp('SPIKE SERVER FAILURE TO CONNECT')
-        disp('***********************************************************')
-        disp('spikeserver failed to connect to plexon PC without erroring')
-        disp('try restarting spikeserver on the plexon PC and then re-run')
-        disp('dv = spikeServerConnect(dv) from the command line on this rig')
-        disp('if that still doesn''t work. check the ip address on both machines')
-        disp('see setupPLDAPSenv.m for instructions on updating spikeserver preferences')
-        pause(2)
+if dv.useSpikeServer
+    try
+        [dv.spikeserver.sock,dv.spikeserver.isConnected, dv.spikeserver.t0, dv.spikeserver.localt0] = udpConnect(dv.spikeserver.selfip,dv.spikeserver.selfport,dv.spikeserver.remoteip,dv.spikeserver.remoteport);
+        if dv.spikeserver.isConnected ==0
+            disp('***********************************************************')
+            disp('SPIKE SERVER FAILURE TO CONNECT')
+            disp('***********************************************************')
+            disp('spikeserver failed to connect to plexon PC without erroring')
+            disp('try restarting spikeserver on the plexon PC and then re-run')
+            disp('dv = spikeServerConnect(dv) from the command line on this rig')
+            disp('if that still doesn''t work. check the ip address on both machines')
+            disp('see setupPLDAPSenv.m for instructions on updating spikeserver preferences')
+            pause(2)
+        end
+    catch me
+        disp('error connecting to spike server')
+        dv.spikeserver.error = me;
+        dv.spikeserver.isConnected = false;
+        dv.useSpikeServer = 0;
     end
-catch me
-    disp('error connecting to spike server')
-    dv.spikeserver.error = me; 
-    dv.spikeserver.isConnected = false; 
 end
