743d93e29d830c18caccafef689e111325983166
Jake <jacoby8s@gmail.com>
2014-01-23 18:06:09 -0600

    only run pdsEyelinkFinish if useEyelink ==1 Also added boolean for saving the edf file

--- SupportFunctions/pdsEyelinkFinish.m
+++ SupportFunctions/pdsEyelinkFinish.m
@@ -1,36 +1,38 @@
 function dv = pdsEyelinkFinish(dv)
 % dv = pdsEyelinkFinish(dv)
-% EyelinkFinish stops recording and closes the currently open edf file. 
+% EyelinkFinish stops recording and closes the currently open edf file.
 % dv [strut]
 
 
 
 if nargin < 1
     help pdsEyelinkFinish
-	return;
+    return;
+end
+if ~isfield(dv.disp, 'saveEDF')
+    dv.disp.saveEDF = 0;
 end
 
-% edfFile = fullfile(dv.el.edfFileLocation, dv.el.edfFile);
-edfFile = dv.el.edfFile;
-Eyelink('StopRecording');
-Eyelink('CloseFile');
-% download data file
-try
-    fprintf('Receiving data file ''%s''\n', edfFile);
-%     status=Eyelink('ReceiveFile', dv.el.edfFile, dv.el.edfFileLocation);
-    status=Eyelink('ReceiveFile', edfFile);
-    if status > 0
-        fprintf('ReceiveFile status %d\n', status);
+if dv.useEyelink && dv.disp.saveEDF
+    % edfFile = fullfile(dv.el.edfFileLocation, dv.el.edfFile);
+    edfFile = dv.el.edfFile;
+    Eyelink('StopRecording');
+    Eyelink('CloseFile');
+    % download data file
+    try
+        fprintf('Receiving data file ''%s''\n', edfFile);
+        %     status=Eyelink('ReceiveFile', dv.el.edfFile, dv.el.edfFileLocation);
+        status=Eyelink('ReceiveFile', edfFile);
+        if status > 0
+            fprintf('ReceiveFile status %d\n', status);
+        end
+        if 2==exist(edfFile, 'file')
+            fprintf('Data file ''%s'' can be found in ''%s''\n', edfFile);
+        end
+        dv.edfData = mglEyelinkEDFRead(edfFile, 1);
+        
+    catch rdf
+        fprintf('Problem receiving data file ''%s''\n', edfFile );
     end
-    if 2==exist(edfFile, 'file')
-        fprintf('Data file ''%s'' can be found in ''%s''\n', edfFile);
-    end
-    dv.edfData = mglEyelinkEDFRead(edfFile, 1);
-    
-catch rdf
-    fprintf('Problem receiving data file ''%s''\n', edfFile );
-    rdf;
 end
 
-
-    
