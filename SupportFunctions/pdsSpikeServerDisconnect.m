062ec0fd6837489e51755b5607709310da96a2c2
Jake <jacoby8s@gmail.com>
2014-02-04 10:04:22 -0600

    fixed spelling error

--- SupportFunctions/pdsSpikeServerDisconnect.m
+++ SupportFunctions/pdsSpikeServerDisconnect.m
@@ -4,7 +4,7 @@ function pdsSpikeServerDisconnect(dv)
 % SPIKESERVERDISCONNECT disconnects spike server 
 
 
-if dv.useSpikeserver && isfield(dv.spikeserver, 'sock')
+if dv.useSpikeServer && isfield(dv.spikeserver, 'sock')
     pnet(dv.spikeserver.sock,'printf',['DISCONNECT' char(10) dv.spikeserver.selfip char(10)]);
     pnet(dv.spikeserver.sock,'write',uint16(dv.spikeserver.selfport));
     pnet(dv.spikeserver.sock,'writepacket',dv.spikeserver.remoteip,dv.spikeserver.remoteport);
