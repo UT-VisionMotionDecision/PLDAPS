dab89e891293910ba015a682dfed5caa2555fa0a
Jake <jacoby8s@gmail.com>
2014-02-04 09:44:02 -0600

    put checks into pdsSpikeServerDisconnect

--- SupportFunctions/pdsSpikeServerDisconnect.m
+++ SupportFunctions/pdsSpikeServerDisconnect.m
@@ -1,10 +1,11 @@
-function spikeServerDisconnect(dv)
+function pdsSpikeServerDisconnect(dv)
 % spikeServerDisconnect(dv)
 %
 % SPIKESERVERDISCONNECT disconnects spike server 
 
 
-
-pnet(dv.spikeserver.sock,'printf',['DISCONNECT' char(10) dv.spikeserver.selfip char(10)]);
-pnet(dv.spikeserver.sock,'write',uint16(dv.spikeserver.selfport));
-pnet(dv.spikeserver.sock,'writepacket',dv.spikeserver.remoteip,dv.spikeserver.remoteport);
+if dv.useSpikeserver && isfield(dv.spikeserver, 'sock')
+    pnet(dv.spikeserver.sock,'printf',['DISCONNECT' char(10) dv.spikeserver.selfip char(10)]);
+    pnet(dv.spikeserver.sock,'write',uint16(dv.spikeserver.selfport));
+    pnet(dv.spikeserver.sock,'writepacket',dv.spikeserver.remoteip,dv.spikeserver.remoteport);
+end
