c4fb668579254ede3814516f90097269a68903be
Jake <jacoby8s@gmail.com>
2014-07-01 12:32:40 -0500

    saving all data as temp files bug fixed

--- SupportFunctions/pdsSaveTempFile.m
+++ SupportFunctions/pdsSaveTempFile.m
@@ -5,21 +5,22 @@ function result = pdsSaveTempFile(dv,PDS)
 result = [];
 
 % extract end of PDS -- takes less than 1ms
-if PDS.trialnumber == 1
+if PDS.trialnumber(end) == 1
     PDStemp = structfun(@(x)  x(end,:), PDS, 'UniformOutput', false);
 else
     PDStemp = structfun(@(x)  getPDSval(x), PDS, 'UniformOutput', false);
+%     PDStemp = structfun(@(x)  x(end,:), PDS, 'UniformOutput', false);
 end
 % if data and timing variables are stored (extract end of those
 % too) -- takes less than 1 ms
 % Sorry - this next part is hacky. Switch to if-else statements
 % instead of try catch - jly
 try
-    PDStemp.timing = structfun(@(x) ( x(end,:) ), PDS.timing, 'UniformOutput', false);
+    PDStemp.timing = structfun(@(x) (getPDSval(x) ), PDS.timing, 'UniformOutput', false);
 catch result
 end
 try
-    PDStemp.data   = structfun(@(x) ( x(end,:) ), PDS.data, 'UniformOutput', false);
+    PDStemp.data   = structfun(@(x) ( getPDSval(x) ), PDS.data, 'UniformOutput', false);
 catch result
 end
 % save -- takes 40 ms
@@ -38,7 +39,7 @@ end
 %%
 
 function val = getPDSval(x)
-    if any(size(x) == 1)  && ~isstr(x) % ie vecotr and not a string
+    if any(size(x) == 1)  && ~ischar(x) % ie vecotr and not a string
         val = x(end);
     else                               % ie matrix or string
         val = x(end,:);     
