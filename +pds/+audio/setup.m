f0c5a5926aa9a783fccc44e3a11a3580690f9e1f
jonaskn <jonasknoell@la-cps844734md-cormack.psy.utexas.edu>
2014-11-24 09:51:10 -0600

    fixes to the audio generalization

--- +pds/+audio/setup.m
+++ +pds/+audio/setup.m
@@ -13,13 +13,13 @@ if p.defaultParameters.sound.use && isField(p.defaultParameters, 'pldaps.dirs.wa
     
     soundDirFiles=dir(soundsDir);
     soundDirFiles={soundDirFiles.name};
-    soundFiles=~isempty(strfind({soundDirFiles,'.wav'));
+    soundFiles=find(~cellfun(@isempty,strfind(soundDirFiles,'.wav')));
     
-    for ifile=soundFiles
-       name= soundDirFiles(soundFiles);
+    for iFile=soundFiles
+       name= soundDirFiles{iFile};
        p.defaultParameters.sound.wavfiles.(name(1:end-4))=fullfile(soundsDir,name);
        
-       [y, freq, ~] = audioread(p.defaultParameters.sound.wavfiles.(name(1:end-4)));
+       [y, freq, ~] = wavread(p.defaultParameters.sound.wavfiles.(name(1:end-4)));
        wav1 = y';
        nChannels1 = size(wav1, 1);
        p.defaultParameters.sound.(name(1:end-4)) = PsychPortAudio('Open', [], [], 1, freq, nChannels1);
