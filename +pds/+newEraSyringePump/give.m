400f28334fd1348a0a53617538aafc551d8bfa87
jonaskn <jonaskn@users.noreply.github.com>
2015-05-14 16:30:22 -0500

    newEraSringePump: ingnore tring to dispense less than 0.001. Would be interpreted as 0, which means continuous dispensing.

--- +pds/+newEraSyringePump/give.m
+++ +pds/+newEraSyringePump/give.m
@@ -8,7 +8,7 @@ function p = give(p, amount)
     if p.trial.newEraSyringePump.use
         if nargin <2 %repeat last given Volume
             IOPort('Write', p.trial.newEraSyringePump.h, ['RUN' p.trial.newEraSyringePump.commandSeparator],0);
-        else
+        elseif amount>=0.001
 %             IOPort('Write', p.trial.newEraSyringePump.h, ['VOL ' num2str(amount) p.trial.newEraSyringePump.commandSeparator],0);
 %             IOPort('Write', p.trial.newEraSyringePump.h, ['RUN' p.trial.newEraSyringePump.commandSeparator],0);
             IOPort('Write', p.trial.newEraSyringePump.h, ['VOL ' sprintf('%3.3f',amount) p.trial.newEraSyringePump.commandSeparator 'RUN' p.trial.newEraSyringePump.commandSeparator],0);
