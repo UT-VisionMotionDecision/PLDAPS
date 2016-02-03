0e77b582e5a391e05044432e879976358443be91
Kate Bonnen <kathryn.bonnen@utexas.edu>
2016-02-03 09:41:27 -0600

    Adding leap SDK and leap PLDAPS code.

---
+++ +pds/+leap/leap.m
@@ -0,0 +1,67 @@
+function [] = leap(p,state)
+    
+    switch state
+        
+        case p.trial.pldaps.trialStates.frameUpdate;
+            frameUpdate(p);
+                
+        case p.trial.pldaps.trialStates.trialCleanUpandSave;
+            cleanUpAndSave(p);
+        
+        case p.trial.pldaps.trialStates.trialSetup;
+            trialSetup(p);    
+    end
+
+end
+
+
+function []= trialSetup(p)
+
+    [p.trial.leap.origin,p.trial.leap.succ] = LeapDevice.getRawCoords();
+    if ~p.trial.leap.succ
+        p.trial.state = p.trial.stimulus.states.NOCURSORINPUT;
+    end
+    
+    p.trial.leap.cursorSamples = nan(3,round(round(p.trial.pldaps.maxFrames*1.1)));
+    p.trial.leap.samplesTimes=nan(1,round(round(p.trial.pldaps.maxFrames*1.1)));
+    p.trial.leap.samples = 0;
+    
+    
+
+end
+
+
+function []= frameUpdate(p)
+
+    [pos,p.trial.leap.succ] = getRawCoords();
+    p.trial.leap.samples = p.trial.leap.samples+1;
+    p.trial.leap.samplesTimes(p.trial.leap.samples)=GetSecs;
+    p.trial.leap.cursorSamples(1:3,p.trial.leap.samples) = pos;
+    
+
+end
+
+function [] = cleanUpAndSave(p)
+
+    p.trial.leap.cursorSamples(:,p.trial.leap.samples+1:end) = [];
+    p.trial.leap.samplesTimes(:,p.trial.leap.samples+1:end) = [];
+
+end
+
+function [pos,succ] = getRawCoords()
+    pos = [NaN,NaN,NaN];
+    succ = true;
+    try
+        f = pds.leap.matleapmaster.matleap(1);
+        x = f.pointables(1).position(1);
+        y = f.pointables(1).position(2);
+        z = f.pointables(1).position(3);
+        pos = [x;y;z];
+        pos
+    catch
+        fprintf('Here\n');
+        
+        succ = false;
+    end
+end
+
