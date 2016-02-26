97546620e005305faa1119dea21052d54afc3aa9
jonaskn <jonaskn@users.noreply.github.com>
2016-02-26 14:02:23 -0600

    changes matleap to use callback. remove sdk mistakenly added by another user

--- +pds/+leap/leap.m
+++ +pds/+leap/leap.m
@@ -16,56 +16,46 @@ end
 
 
 function []= trialSetup(p)
-
+    %start listening
+    pds.leap.matleapmaster.matleap(2);
     
-    [p.trial.leap.origin,p.trial.leap.succ] = getRawCoords();
-    p.trial.leap.origin=[0;250;0];
-    p.trial.leap.succ=1;
-    if ~p.trial.leap.succ
-        p.trial.state = p.trial.stimulus.states.NOCURSORINPUT;
-    end
-    
-    p.trial.leap.cursorSamples = nan(3,round(round(p.trial.pldaps.maxFrames*1.1)));
-    p.trial.leap.samplesTimes=nan(1,round(round(p.trial.pldaps.maxFrames*1.1)));
+    %allocate data
+    %assuming leap sampling rate o 120Hz for now. Could query later if needed.
+    p.trial.leap.cursorSamples = nan(3,round(round(p.trial.pldaps.maxFrames/p.trial.display.frate*120*1.1)));
+    p.trial.leap.samplesTimes = nan(3,round(round(p.trial.pldaps.maxFrames/p.trial.display.frate*120*1.1)));
     p.trial.leap.samples = 0;
-    
-    
-
 end
 
 
 function []= frameUpdate(p)
-
-    [pos,p.trial.leap.succ] = getRawCoords();
-    p.trial.leap.samples = p.trial.leap.samples+1;
-    p.trial.leap.samplesTimes(p.trial.leap.samples)=GetSecs;
-
-    p.trial.leap.cursorSamples(1:3,p.trial.leap.samples) = pos-p.trial.leap.origin;
-    if ~p.trial.leap.succ
-        p.trial.state = p.trial.stimulus.states.NOCURSORINPUT;
-    end
+    getRawCoords(p);
 end
 
 function [] = cleanUpAndSave(p)
-
+    %stop listening
+    pds.leap.matleapmaster.matleap(3);
+    %prune unused data
     p.trial.leap.cursorSamples(:,p.trial.leap.samples+1:end) = [];
     p.trial.leap.samplesTimes(:,p.trial.leap.samples+1:end) = [];
 
 end
 
-function [pos,succ] = getRawCoords()
-
-    pos = [NaN;NaN;NaN];
-    succ = true;
-    try
-        f = pds.leap.matleapmaster.matleap(1);
-        x = f.pointables(1).position(1);
-        y = f.pointables(1).position(2);
-        z = f.pointables(1).position(3);
-        pos = [x;y;z];
-
-    catch
-        succ = false;
+function getRawCoords(p)
+    succ = false;
+    f = pds.leap.matleapmaster.matleap(1);
+    fpcell={f.pointables};
+    if ~isempty(f)
+        has_pointables=~cellfun(@isempty, fpcell);
+        nsamples=sum(has_pointables);
+        if nsamples>0
+            succ=true;
+            pos=cellfun(@(x) x(1).position, fpcell(has_pointables), 'UniformOutput', false);
+            pos=vertcat(pos{:})';
+            p.trial.leap.cursorSamples(1:3,p.trial.leap.samples+(1:nsamples))=pos;
+            p.trial.leap.samplesTimes(1:3,p.trial.leap.samples+(1:nsamples))=[f(has_pointables).getsecs; f(has_pointables).timestamp; f(has_pointables).id];
+            p.trial.leap.samples=p.trial.leap.samples+nsamples;
+        end
     end
+    p.trial.leap.succ = succ;
 end
 
