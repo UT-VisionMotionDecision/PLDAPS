1666fc92b1e471260f4daf6352cb3c816d472af3
Jacob Yates <jacoby8s@gmail.com>
2016-09-27 15:54:41 -0400

    more robust. had weird column vector error with for loop

--- @pldaps/runStateforModules.m
+++ @pldaps/runStateforModules.m
@@ -1,12 +1,14 @@
 function runStateforModules(p,state,modules,moduleFunctionHandles,moduleRequestedStates,moduleLocationInputs)
- 
+
 %using cellfun might be slower than a for loop and does not guaranty
 %execution in order of the arrays. Not sure if save. Ideally it would be
 %     cellfun(@(x) x(p,p.trial.pldaps.trialStates.(state)),modules(moduleRequestedStates.(state)));
-for iModule=find(moduleRequestedStates.(state))
+
+tmpModules=moduleRequestedStates.(state);
+for iModule=find(tmpModules(:))'
     if moduleLocationInputs(iModule)
-        moduleFunctionHandles{iModule}(p,p.trial.pldaps.trialStates.(state),modules{iModule}); 
+        moduleFunctionHandles{iModule}(p,p.trial.pldaps.trialStates.(state),modules{iModule});
     else
-        moduleFunctionHandles{iModule}(p,p.trial.pldaps.trialStates.(state)); 
+        moduleFunctionHandles{iModule}(p,p.trial.pldaps.trialStates.(state));
     end
 end
