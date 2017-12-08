24ca80ed08b20b6c2c3ceae3f2bb6df6b0bf0bd4
czuba <czuba@utexas.edu>
2017-12-08 17:50:30 -0600

    nothing much...thought I was in glDraw branch, but might as well push this minor tweak here

--- @pldaps/run.m
+++ @pldaps/run.m
@@ -48,10 +48,17 @@ function p = run(p)
             p.defaultParameters.session.file = cfile;
         end
 
-         if ~exist(p.trial.session.dir, 'dir')
-            warning('pldaps:run','Data directory specified in .pldaps.dirs.data does not exist. Quitting PLDAPS. p.trial.pldaps.dirs.data=%s\nPlease create the directory along with a subdirectory called TEMP',p.trial.session.dir);
-            return;
-         end
+        if ~exist(p.trial.session.dir, 'dir')
+            warning('pldaps:run','Data directory specified in .pldaps.dirs.data does not exist.\n');
+            ans = input(sprintf('\tShould I create Data & TEMP dirs in: %s? (...if not, will quit PLDAPS)\n\t\t(y/n): ',p.trial.pldaps.dirs.data), 's');
+            if ~isempty(ans) && lower(ans(1))=='y'
+                mkdir(fullfile(p.trial.pldaps.dirs.data));
+                mkdir(fullfile(p.trial.pldaps.dirs.data, 'TEMP'));
+            else
+                fprintf(2, '\n\tQuitting PLDAPS. Please run createRigPrefs to update your data dirs,\n\tor create directory %s, containing a subdirectory called ''TEMP''\n\n', p.trial.pldaps.dirs.data)
+                return;
+            end
+        end
     else
         p.defaultParameters.session.file='';
         p.defaultParameters.session.dir='';
