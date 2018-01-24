9bf463db7f2a734426809fce6df7d85a67e691b9
czuba <czuba@utexas.edu>
2018-01-24 11:45:48 -0600

    minor cleanup from linux setup

--- SupportFunctions/Utils/addToPathWithoutGit.m
+++ SupportFunctions/Utils/addToPathWithoutGit.m
@@ -1,17 +1,21 @@
 function addToPathWithoutGit(dir, excludes)
     a=genpath(dir);
-    b=textscan(a,'%s','delimiter',':');
-    b=b{1};
-    b(~cellfun(@isempty,strfind(b,'.git')))=[];
-    b(~cellfun(@isempty,strfind(b,'.svn')))=[];
-    if nargin>1
-        if ~iscell(excludes), excludes = {excludes}; end
-        for i = 1:numel(excludes)
-            if ~isempty(excludes{i})
-                b(~cellfun(@isempty,strfind(b, excludes{i})))=[];
+    if isempty(a)
+        fprintf('%s not found...attempting to continue\n', dir);
+    else
+        b=textscan(a,'%s','delimiter',':');
+        b=b{1};
+        b(~cellfun(@isempty,strfind(b,'.git')))=[];
+        b(~cellfun(@isempty,strfind(b,'.svn')))=[];
+        if nargin>1
+            if ~iscell(excludes), excludes = {excludes}; end
+            for i = 1:numel(excludes)
+                if ~isempty(excludes{i})
+                    b(~cellfun(@isempty,strfind(b, excludes{i})))=[];
+                end
             end
         end
+        addpath(b{:})
+        disp([dir ' and subdirectories added to the path']);
     end
-    addpath(b{:})
-    disp([dir ' and subdirectories added to the path']);
 end
