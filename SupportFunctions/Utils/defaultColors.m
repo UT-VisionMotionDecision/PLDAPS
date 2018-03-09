00ed33241ebbb9a21b13b54a66de0ff95d7a9f15
Jake <jacoby8s@gmail.com>
2018-03-09 14:33:25 -0500

    fixed color indices for transparency

--- SupportFunctions/Utils/defaultColors.m
+++ SupportFunctions/Utils/defaultColors.m
@@ -44,7 +44,7 @@ p.defaultParameters.display.monkeyCLUT = [0,0,0; % IGNORE THIS LINE (CLUT is 0 b
     zeros(241,3)];
 
 %%% keeping track of which color is which
-if p.defaultParameters.display.useOverlay %TODO make this separate from useDatapixx %(p.defaultParameters.datapixx.use && p.defaultParameters.display.useOverlay)
+if p.defaultParameters.display.useOverlay == 1 %TODO make this separate from useDatapixx %(p.defaultParameters.datapixx.use && p.defaultParameters.display.useOverlay)
     p.defaultParameters.display.clut.bg         = 1*[1 1 1]';
     p.defaultParameters.display.clut.cursor     = 2*[1 1 1]';
     p.defaultParameters.display.clut.targetgood = 3*[1 1 1]';
@@ -60,21 +60,21 @@ if p.defaultParameters.display.useOverlay %TODO make this separate from useDatap
     p.defaultParameters.display.clut.redbg      = 13*[1 1 1]';
     p.defaultParameters.display.clut.blackbg    = 14*[1 1 1]';
     
-elseif p.defaultParameters.display.useOverlay %TODO add switch so choose between monkey and human clut
-   p.defaultParameters.display.clut.bg         = p.defaultParameters.display.humanCLUT(1+1,:)';
-   p.defaultParameters.display.clut.cursor     = p.defaultParameters.display.humanCLUT(2+1,:)';
-   p.defaultParameters.display.clut.targetgood = p.defaultParameters.display.humanCLUT(3+1,:)';
-   p.defaultParameters.display.clut.targetnull = p.defaultParameters.display.humanCLUT(4+1,:)';
-   p.defaultParameters.display.clut.window     = p.defaultParameters.display.humanCLUT(5+1,:)';
-   p.defaultParameters.display.clut.fixation   = p.defaultParameters.display.humanCLUT(6+1,:)';
-   p.defaultParameters.display.clut.white      = p.defaultParameters.display.humanCLUT(7+1,:)';
-   p.defaultParameters.display.clut.eyepos     = p.defaultParameters.display.humanCLUT(8+1,:)';
-   p.defaultParameters.display.clut.black      = p.defaultParameters.display.humanCLUT(9+1,:)';
-   p.defaultParameters.display.clut.blue       = p.defaultParameters.display.humanCLUT(10+1,:)';
-   p.defaultParameters.display.clut.red        = p.defaultParameters.display.humanCLUT(4+1,:)';
-   p.defaultParameters.display.clut.greenbg    = p.defaultParameters.display.humanCLUT(12+1,:)';
-   p.defaultParameters.display.clut.redbg      = p.defaultParameters.display.humanCLUT(13+1,:)';
-   p.defaultParameters.display.clut.blackbg    = p.defaultParameters.display.humanCLUT(14+1,:)';
+elseif p.defaultParameters.display.useOverlay == 2
+       p.defaultParameters.display.clut.bg      = 1*[1 1 1];
+    p.defaultParameters.display.clut.cursor     = 2*[1 1 1];
+    p.defaultParameters.display.clut.targetgood = 3*[1 1 1];
+    p.defaultParameters.display.clut.targetnull = 4*[1 1 1];
+    p.defaultParameters.display.clut.window     = 5*[1 1 1];
+    p.defaultParameters.display.clut.fixation   = 6*[1 1 1];
+    p.defaultParameters.display.clut.white      = 7*[1 1 1];
+    p.defaultParameters.display.clut.eyepos     = 8*[1 1 1];
+    p.defaultParameters.display.clut.black      = 9*[1 1 1];
+    p.defaultParameters.display.clut.blue       = 10*[1 1 1];
+    p.defaultParameters.display.clut.red        = 4*[1 1 1];
+    p.defaultParameters.display.clut.greenbg    = 12*[1 1 1];
+    p.defaultParameters.display.clut.redbg      = 13*[1 1 1];
+    p.defaultParameters.display.clut.blackbg    = 14*[1 1 1];
 else %TODO add switch so choose between monkey and human clut
    p.defaultParameters.display.clut.bg         = p.defaultParameters.display.monkeyCLUT(1+1,:)';
    p.defaultParameters.display.clut.cursor     = p.defaultParameters.display.monkeyCLUT(2+1,:)';
