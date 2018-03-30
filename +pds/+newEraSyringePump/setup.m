f355bb53c346262f1327103bd7679750b3ce298b
czuba <czuba@utexas.edu>
2018-03-29 19:48:52 -0500

    binocular eyelink setup & newEraSyringePump setup doc

--- +pds/+newEraSyringePump/setup.m
+++ +pds/+newEraSyringePump/setup.m
@@ -8,49 +8,68 @@ function p = setup(p)
 % jk wrote it 2015
 
 if p.trial.newEraSyringePump.use
-    cs='BaudRate=19200 DTR=1 RTS=1 ReceiveTimeout=1';
+    config='BaudRate=19200 DTR=1 RTS=1 ReceiveTimeout=1'; % orig
+    %config= [config, ' Terminator=13 ProcessingMode=Cooked'];
     % Nooo. All IOPort connections now closed at start of p.run
     %     IOPort('closeAll'); %risky. for now....
     %     WaitSecs(0.1);
-    [h, errmsg]=IOPort('OpenSerialPort',p.trial.newEraSyringePump.port,cs);%'/dev/cu.usbserial'
+    
+    %% Open port
+    [h, errmsg]=IOPort('OpenSerialPort', p.trial.newEraSyringePump.port, config);% Mac:'/dev/cu.usbserial' Linux:'/dev/ttyUSB0'
     WaitSecs(0.1);
     if ~isempty(errmsg)
         error('pds:newEraSyringePump:setup',['Failed to open serial Port with message ' char(10) errmsg]);
     end
     p.trial.newEraSyringePump.h = h;
     
+    %% Configure pump
+    % serial com line terminator
     p.trial.newEraSyringePump.commandSeparator = [char(13) repmat(char(10),1,20)];
-
+    
+    % flush serial command pipeline (no command)
     IOPort('Write', h, [p.trial.newEraSyringePump.commandSeparator],0);
+    % set syringe diameter (...but don't yet, because value is empty here. Why??? --TBC)
     IOPort('Write', h, ['DIA' p.trial.newEraSyringePump.commandSeparator],0);%0.05
-    
+    % set pumping direction to INFuse   (INF==infuse, WDR==withdraw, REV==reverse current dir)
     IOPort('Write', h, ['DIR INF'  p.trial.newEraSyringePump.commandSeparator],0);
-
+    % enable/disable low-noise mode (logical, attempts to reduce high-freq noise from slow pump rates...unk. effect/utility in typical ephys enviro. --TBC)
     IOPort('Write', h, ['LN ' num2str(p.trial.newEraSyringePump.lowNoiseMode) p.trial.newEraSyringePump.commandSeparator],0); %low noise mode, try
-    
+    % enable/disable audible alarm state (0==off, 1==on/use)
     IOPort('Write', h, ['AL ' num2str(p.trial.newEraSyringePump.alarmMode) p.trial.newEraSyringePump.commandSeparator],0); %low noise mode, try
-
+    % set TTL trigger mode ('T2'=="Rising edge starts pumping program";  see NE-500 user manual for other options & descriptions)
     IOPort('Write', h, ['TRG ' p.trial.newEraSyringePump.triggerMode  p.trial.newEraSyringePump.commandSeparator],0);
     
+    %%
     WaitSecs(0.1);
+    % Read out current syringe diameter before applying any changes
+    %       why?...dia. changes will zero out machine 'volume dispensed' & would erase record between Pldaps files in a session
     a=char(IOPort('Read',h,1,14));
     currentDiameter=str2double(a(10:end));
-    
-    if currentDiameter~=p.trial.newEraSyringePump.diameter
+    % Warn if different
+    while currentDiameter~=p.trial.newEraSyringePump.diameter
         if p.trial.newEraSyringePump.allowNewDiameter
-            IOPort('Write', h, ['DIA ' num2str(p.trial.newEraSyringePump.diameter) p.trial.newEraSyringePump.commandSeparator],0); 
+            IOPort('Write', h, ['DIA ' num2str(p.trial.newEraSyringePump.diameter) p.trial.newEraSyringePump.commandSeparator],0);
+            % Refresh currentDiameter reported by pump
+            WaitSecs(0.1);
+            IOPort('Write', h, ['DIA' p.trial.newEraSyringePump.commandSeparator],0);
+            WaitSecs(0.1);
+            a=char(IOPort('Read',h,1,14));
+            currentDiameter=str2double(a(10:end));
         else
-            error('pds:newEraSyringePump:setup','Change in Diametersize requested. Please confirm that you want to do this, as it would zero the current volume settings')
+            %error('pds:newEraSyringePump:setup','Change in Diametersize requested. Please confirm that you want to do this, as it would zero the current volume settings')
+            fprintf(2, '!!!\t Change in Diametersize requested.\n!!!\tDoing so would zero out the current volume settings & information [for this session]\n\nTo confirm that you want to do this, set \n\tp.trial.newEraSyringePump.allowNewDiameter = true;\n\n');
+            keyboard
         end
     end
-    
+    %%
+    % set pumping rate & units (def: 2900, 'MH')        ['MH'==mL/hour]
     IOPort('Write', h, ['RAT ' num2str(p.trial.newEraSyringePump.rate) ' MH ' p.trial.newEraSyringePump.commandSeparator],0);%2900
-
-     
+    % set reward volume & units (def: 0.05, 'ML')
     IOPort('Write', h, ['VOL ' num2str(p.trial.behavior.reward.defaultAmount) ' ' p.trial.newEraSyringePump.volumeUnits p.trial.newEraSyringePump.commandSeparator],0);%0.05
     
     
 %     a=char(IOPort('Read',h)); %clear buffer
+
     %now read and store the current Volumes at the beginning of the
     %experiment. Don't reset to allow a volume that caputre the volume
     %dispensed over a whole session
--- SupportFunctions/Utils/defaultColors.m
+++ SupportFunctions/Utils/defaultColors.m
@@ -4,44 +4,53 @@ function p = defaultColors(p)
 % these later as long as it's done before pdsDatapixxInit
 
 % 12/13/2013 jly    Wrote it
+% 2018-03-28  TBC   Polished, binocular compat.
 
+bgColor = p.defaultParameters.display.bgColor;
+clutDepth = 2^8;
 
 %%% CLUT colors %%%
 % note: CLUT indexing and MATLAB indexing are zero and 1-based, respectively
 p.defaultParameters.display.humanCLUT = [0, 0, 0;  % IGNORE THIS LINE
-    p.defaultParameters.display.bgColor; % bg           1
-    0.8, 0, 0.5;  % cursor          2
-    0, 1, 0;  % target color        3
-    1, 0, 0;  % null target color   4
-    1, 1, 1;  % window color        5
-    1, 0, 0;  % fixation color      6
-    1, 1, 1;  % white (dots)        7
-    0, 1, 1;  % eye (turqoise)      8
-    0, 0, 0;  % black (dots)        9
-    0,0,1;  % blue                  10
-    1,0,0; % red/green              11
-    0,1,0;  % green/bg               12
-    1,0,0;  % red/bg                 13
-    0,0,0;  % black/bg               14
-    zeros(241,3)];
+    bgColor;    % background            1
+    0.8, 0, 0.5;% cursor                2
+    0, 1, 0;    % target color          3
+    1, 0, 0;    % null target color     4
+    1, 1, 1;    % window color          5
+    1, 0, 0;    % fixation color        6
+    1, 1, 1;    % white (dots)          7
+    0, 1, 1;    % eye1 (turqoise)       8
+    0, 0, 0;    % black (dots)          9
+    0, 0, 1;    % blue                  10
+    1, 0, 0;    % red/green             11
+    0, 1, 0;    % green/bg              12
+    1, 0, 0;    % red/bg                13
+    0, 0, 0;    % black/bg              14
+    0, 0.7, 1]; % eye2 (blue-green)     15
+    
+    % extend clut length to clutDepth (8-bit)
+    p.defaultParameters.display.humanCLUT(clutDepth, 1:3) = 0;
 
 
 p.defaultParameters.display.monkeyCLUT = [0,0,0; % IGNORE THIS LINE (CLUT is 0 based)
-    p.defaultParameters.display.bgColor;     % bg (gray)          1
-    p.defaultParameters.display.bgColor;     % cursor (bg)        2
-    1, 0, 0;             % target color       3
-    1, 0, 0;             % null target color  4
-    p.defaultParameters.display.bgColor;     % window color       5
-    1, 0, 0;             % fixation color     6
-    1, 1, 1;             % white (dots)       7
-    p.defaultParameters.display.bgColor;     % eyepos (bg)        8
-    0,0,0                % black (dots)       9
-    0,0,1;               % blue               10
-    0,1,0; % red/green              11
-    p.defaultParameters.display.bgColor;  % green/bg               12
-    p.defaultParameters.display.bgColor;  % red/bg                 13
-    p.defaultParameters.display.bgColor;  % black/bg               14
-    zeros(241,3)];
+    bgColor;    % bg (grey)             1
+    bgColor;    % cursor (bg)           2
+    1, 0, 0;    % target color          3
+    1, 0, 0;    % null target color     4
+    bgColor;    % window color          5
+    1, 0, 0;    % fixation color        6
+    1, 1, 1;    % white (dots)          7
+    bgColor;    % eyepos (bg)           8
+    0,0,0       % black (dots)          9
+    0,0,1;      % blue                  10
+    0,1,0;      % red/green             11
+    bgColor;    % green/bg              12
+    bgColor;    % red/bg                13
+    bgColor;    % black/bg              14
+    bgColor];   % eye2 (bg)             15
+
+    % extend clut length to clutDepth (8-bit)
+    p.defaultParameters.display.monkeyCLUT(clutDepth, 1:3) = 0;
 
 %%% keeping track of which color is which
 if p.defaultParameters.display.useOverlay %TODO make this separate from useDatapixx %(p.defaultParameters.datapixx.use && p.defaultParameters.display.useOverlay)
@@ -59,35 +68,42 @@ if p.defaultParameters.display.useOverlay %TODO make this separate from useDatap
     p.defaultParameters.display.clut.greenbg    = 12*[1 1 1]';
     p.defaultParameters.display.clut.redbg      = 13*[1 1 1]';
     p.defaultParameters.display.clut.blackbg    = 14*[1 1 1]';
+    p.defaultParameters.display.clut.eye1       = 8*[1 1 1]';
+    p.defaultParameters.display.clut.eye2       = 15*[1 1 1]';
     
 elseif p.defaultParameters.display.useOverlay %TODO add switch so choose between monkey and human clut
-   p.defaultParameters.display.clut.bg         = p.defaultParameters.display.humanCLUT(1+1,:)';
-   p.defaultParameters.display.clut.cursor     = p.defaultParameters.display.humanCLUT(2+1,:)';
-   p.defaultParameters.display.clut.targetgood = p.defaultParameters.display.humanCLUT(3+1,:)';
-   p.defaultParameters.display.clut.targetnull = p.defaultParameters.display.humanCLUT(4+1,:)';
-   p.defaultParameters.display.clut.window     = p.defaultParameters.display.humanCLUT(5+1,:)';
-   p.defaultParameters.display.clut.fixation   = p.defaultParameters.display.humanCLUT(6+1,:)';
-   p.defaultParameters.display.clut.white      = p.defaultParameters.display.humanCLUT(7+1,:)';
-   p.defaultParameters.display.clut.eyepos     = p.defaultParameters.display.humanCLUT(8+1,:)';
-   p.defaultParameters.display.clut.black      = p.defaultParameters.display.humanCLUT(9+1,:)';
-   p.defaultParameters.display.clut.blue       = p.defaultParameters.display.humanCLUT(10+1,:)';
-   p.defaultParameters.display.clut.red        = p.defaultParameters.display.humanCLUT(4+1,:)';
-   p.defaultParameters.display.clut.greenbg    = p.defaultParameters.display.humanCLUT(12+1,:)';
-   p.defaultParameters.display.clut.redbg      = p.defaultParameters.display.humanCLUT(13+1,:)';
-   p.defaultParameters.display.clut.blackbg    = p.defaultParameters.display.humanCLUT(14+1,:)';
+    p.defaultParameters.display.clut.bg         = p.defaultParameters.display.humanCLUT(1+1,:)';
+    p.defaultParameters.display.clut.cursor     = p.defaultParameters.display.humanCLUT(2+1,:)';
+    p.defaultParameters.display.clut.targetgood = p.defaultParameters.display.humanCLUT(3+1,:)';
+    p.defaultParameters.display.clut.targetnull = p.defaultParameters.display.humanCLUT(4+1,:)';
+    p.defaultParameters.display.clut.window     = p.defaultParameters.display.humanCLUT(5+1,:)';
+    p.defaultParameters.display.clut.fixation   = p.defaultParameters.display.humanCLUT(6+1,:)';
+    p.defaultParameters.display.clut.white      = p.defaultParameters.display.humanCLUT(7+1,:)';
+    p.defaultParameters.display.clut.eyepos     = p.defaultParameters.display.humanCLUT(8+1,:)';
+    p.defaultParameters.display.clut.black      = p.defaultParameters.display.humanCLUT(9+1,:)';
+    p.defaultParameters.display.clut.blue       = p.defaultParameters.display.humanCLUT(10+1,:)';
+    p.defaultParameters.display.clut.red        = p.defaultParameters.display.humanCLUT(4+1,:)';
+    p.defaultParameters.display.clut.greenbg    = p.defaultParameters.display.humanCLUT(12+1,:)';
+    p.defaultParameters.display.clut.redbg      = p.defaultParameters.display.humanCLUT(13+1,:)';
+    p.defaultParameters.display.clut.blackbg    = p.defaultParameters.display.humanCLUT(14+1,:)';
+    p.defaultParameters.display.clut.eye1       = p.defaultParameters.display.humanCLUT(8+1,:)';
+    p.defaultParameters.display.clut.eye2       = p.defaultParameters.display.humanCLUT(15+1,:)';
+    
 else %TODO add switch so choose between monkey and human clut
-   p.defaultParameters.display.clut.bg         = p.defaultParameters.display.monkeyCLUT(1+1,:)';
-   p.defaultParameters.display.clut.cursor     = p.defaultParameters.display.monkeyCLUT(2+1,:)';
-   p.defaultParameters.display.clut.targetgood = p.defaultParameters.display.monkeyCLUT(3+1,:)';
-   p.defaultParameters.display.clut.targetnull = p.defaultParameters.display.monkeyCLUT(4+1,:)';
-   p.defaultParameters.display.clut.window     = p.defaultParameters.display.monkeyCLUT(5+1,:)';
-   p.defaultParameters.display.clut.fixation   = p.defaultParameters.display.monkeyCLUT(6+1,:)';
-   p.defaultParameters.display.clut.white      = p.defaultParameters.display.monkeyCLUT(7+1,:)';
-   p.defaultParameters.display.clut.eyepos     = p.defaultParameters.display.monkeyCLUT(8+1,:)';
-   p.defaultParameters.display.clut.black      = p.defaultParameters.display.monkeyCLUT(9+1,:)';
-   p.defaultParameters.display.clut.blue       = p.defaultParameters.display.monkeyCLUT(10+1,:)';
-   p.defaultParameters.display.clut.red        = p.defaultParameters.display.monkeyCLUT(4+1,:)';
-   p.defaultParameters.display.clut.greenbg    = p.defaultParameters.display.monkeyCLUT(12+1,:)';
-   p.defaultParameters.display.clut.redbg      = p.defaultParameters.display.monkeyCLUT(13+1,:)';
-   p.defaultParameters.display.clut.blackbg    = p.defaultParameters.display.monkeyCLUT(14+1,:)';
+    p.defaultParameters.display.clut.bg         = p.defaultParameters.display.monkeyCLUT(1+1,:)';
+    p.defaultParameters.display.clut.cursor     = p.defaultParameters.display.monkeyCLUT(2+1,:)';
+    p.defaultParameters.display.clut.targetgood = p.defaultParameters.display.monkeyCLUT(3+1,:)';
+    p.defaultParameters.display.clut.targetnull = p.defaultParameters.display.monkeyCLUT(4+1,:)';
+    p.defaultParameters.display.clut.window     = p.defaultParameters.display.monkeyCLUT(5+1,:)';
+    p.defaultParameters.display.clut.fixation   = p.defaultParameters.display.monkeyCLUT(6+1,:)';
+    p.defaultParameters.display.clut.white      = p.defaultParameters.display.monkeyCLUT(7+1,:)';
+    p.defaultParameters.display.clut.eyepos     = p.defaultParameters.display.monkeyCLUT(8+1,:)';
+    p.defaultParameters.display.clut.black      = p.defaultParameters.display.monkeyCLUT(9+1,:)';
+    p.defaultParameters.display.clut.blue       = p.defaultParameters.display.monkeyCLUT(10+1,:)';
+    p.defaultParameters.display.clut.red        = p.defaultParameters.display.monkeyCLUT(4+1,:)';
+    p.defaultParameters.display.clut.greenbg    = p.defaultParameters.display.monkeyCLUT(12+1,:)';
+    p.defaultParameters.display.clut.redbg      = p.defaultParameters.display.monkeyCLUT(13+1,:)';
+    p.defaultParameters.display.clut.blackbg    = p.defaultParameters.display.monkeyCLUT(14+1,:)';
+    p.defaultParameters.display.clut.eye1       = p.defaultParameters.display.monkeyCLUT(8+1,:)';
+    p.defaultParameters.display.clut.eye2       = p.defaultParameters.display.monkeyCLUT(15+1,:)';
 end
