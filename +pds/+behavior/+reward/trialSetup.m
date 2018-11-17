33b8714ec65e50c2d92c97dfe2072c961f98cb03
czuba <czuba@utexas.edu>
2018-11-16 20:26:53 -0600

    hanging chad

--- +pds/+behavior/+reward/trialSetup.m
+++ +pds/+behavior/+reward/trialSetup.m
@@ -1,4 +1,4 @@
 function p = trialSetup(p)
 %pds.behavior.reward.trialSetup(p)    allocate memory for use during the trial
     p.trial.behavior.reward.iReward     = 1; % counter for reward times
-    p.trial.behavior.reward.timeReward  = nan(2,p.trial.pldaps.maxTrialLength*2); %preallocate for a reward up to every 0.5 s
+    p.trial.behavior.reward.timeReward  = nan(2, ceil(p.trial.pldaps.maxTrialLength*2)); %preallocate for a reward up to every 0.5 s
